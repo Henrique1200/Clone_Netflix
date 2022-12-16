185c1487c4cbeec2c1b540c2f083f7ce6c51feff
Henrique Leonardo <97650427+Henrique1200@users.noreply.github.com>
2022-12-15 21:26:31 -0300

    Add files via upload

---
+++ js/api.js
@@ -0,0 +1,7 @@
+let api_key = "a33dad8a5e28b52df4d3f3513393fcff"
+
+let img_url = "https://image.tmdb.org/t/p/w500/"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
